dd0c9c4cac7c64bcc727596725a49ec8ff787ded
Anthony Graniero <ananfro@gmail.com>
2018-11-08 13:09:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,22 +19,13 @@ class Song
   def self.new_by_filename(filename)
     file = filename.split(" - ")
     song = self.new(file[1])
-   
-  end
-  
- 
-end
-
-  def self.new_by_filename(filename)
-    file = filename.split(" - ")
-    song = self.new(file[1])
     song.artist_name = file[0]
     song
-    #binding.pry
+   
   end
 
   def artist=(artist)
-    #binding.pry
+    
     @artist = artist unless @artist
     artist.add_song(self) unless artist.songs.include?(self)
   end
@@ -42,8 +33,8 @@ end
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
   end
-
-  def self.all
-    @@all
-  end
+ 
 end
+
+
+
